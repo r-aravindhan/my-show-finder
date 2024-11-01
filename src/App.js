cd9044462c9538f4417deee8dbd6a5f1af5c7425
Aravindhan Ravichandran <aravindhan_ravichandran@epam.com>
2024-11-01 20:21:11 +0530

    added login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/login/Login";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <span>My Show Finder</span>
+        </div>
+        <br />
+        <div>Book it, love it, relive it!</div>
       </header>
+      <br />
+      <Login />
     </div>
   );
 }
